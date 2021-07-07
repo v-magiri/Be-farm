13717bda04d2a75e6414378e8c8590945ca492ba
vicky-magiri <ndung'u.victor19@students.dkut.ac.ke>
2021-07-07 19:41:07 +0300

    Added welcome page

--- php/index.php
+++ php/index.php
@@ -1,21 +1,69 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Test</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
-    </head>
-    <body>
-        <form action="index.php">
-            <label for="username">Username</label><br>
-            <input type="text" required id="username" name="username"><br>
-            <label for="pwd">Password</label><br>
-            <input type="password" required id="pwd" name="password"><br>
-            <br>
-            <input type="Submit" name="login" value="Sign in">
-        </form>
-
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/default.css" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     
-    </body>
+    <title>Be-Farm</title>
+
+</head>
+<body>
+    <nav class="nav_bar">
+        <a href="index.php"><img src="../Images/profile.png"></a>
+        <div class="navlink">
+            <ul>
+                <li><a href="signin.php"><i class="fa fa-user"></i> Login</a></li>
+                <li><a href="signup.php"><i class="fa fa-user-plus"></i> Register</a></li>
+            </ul>
+        </div>
+    </nav>
+    <section class="header">
+        <div class="txt">
+            <h1>Be-Farm</h1>
+            <p>Supporting farmers to produce high yields thorugh the use of modern and
+                 environmental friendly farming practices</p>
+            <a href="signup.php" class="btnstr">Get Started</a>
+        </div>
+
+    </section>
+    <section class="serv">
+        <h3 class="serv-header">Services</h3>
+        <div class="row">
+            <div class="servtype">
+                <h3>Agricultural Training</h3>
+                <p>We offer mobile training materials on good Agricultural practices</p>
+            </div>
+            <div class="servtype">
+                <h3>Marketing</h3>
+                <p>We support farmers access market for their produce </p>
+            </div>
+            <div class="servtype">
+                <h3>Farm Technology</h3>
+                <p>We support farmers access quality planting materials and planting </p>
+            </div>
+        </div>
+    </section>
+    <section class="footer">
+            <div class="rowftr">
+                <div class="ftritm">
+                    <p>&copy Be-Farm</p>
+                </div>
+                <div class="ftritm">
+                    <p><i class="fab fa-facebook"></i> Be-Farm</p>
+                </div>
+                <div class="ftritm">
+                    <p><i class="fab fa-twitter"></i> @Be-farm</p>
+                </div>
+                <div class="ftritm">
+                    <p> <a href="mailto:befarm@gmail.com"><i class="fas fa-envelope-square"></i> Mail Us </a></p>
+                </div>
+            </div>
+    </section>
+    
+    
+    
+</body>
 </html>
