662cc4169c718167a52af53a221aa6f0dcdd0c40
vicky-magiri <ndung'u.victor19@students.dkut.ac.ke>
2021-07-14 22:28:08 +0300

    initial farmer functionality

--- src/pages/ask-an-expert.php
+++ src/pages/ask-an-expert.php
@@ -19,10 +19,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../CSS/style.css">
-    <title>Befarm</title>
+    <title>Ask an Expert</title>
 </head>
 <body>
-    <div class="nav_bar">
+<div class="nav_bar">
         <nav>
             <a href="home.php"><img src="../Images/default.png" alt="Befarm" width="50px" style="border-radius:50%"></a>
             <div class="nav-link" id="nav_link">
@@ -52,52 +52,6 @@
             <i class="fa fa-bars" onclick="showMenu()"></i>
         </nav>
     </div>
-    <section class="header">     
-    </section>
-   <!-- to add climate smart crops recommendation -->
-    <section class="traing">
-    <h1>Services</h1>
-    <div class="training">
-    <div class="traing_type">
-    <h3>Farm technology training</h3>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio animi sapiente similique cum unde, facilis maxime, optio id saepe eaque molestiae! Voluptatum ad possimus accusamus cumque expedita iste at fuga!</p>  
-    </div>
-    <div class="traing_type">
-    <h3>Farm Management training</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis porro alias inventore explicabo. Alias provident tenetur architecto officia reiciendis a ab vel, distinctio sit dolor, ea velit amet repellat eligendi?</p>  
-    </div>
-    <div class="traing_type">
-    <h3>Cultivation training</h3>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis porro alias inventore explicabo. Alias provident tenetur architecto officia reiciendis a ab vel, distinctio sit dolor, ea velit amet repellat eligendi?</p>  
-    </div>
-    </div>
-    </section>
-    <!-- farm produce market -->
-    <section class="market">
-        <h2 class="row-header">Farm Produce Market</h2>
-        <div class="marketrow">
-            <div class="markettype">
-                <h3>Mkulima Young</h3>
-                <img src="" alt="Mkulima Young">
-                <p></p>
-                <a href="">Visit Site</a>
-            </div>
-            <div class="markettype">
-                <h3>Jiji Kenya</h3>
-                <img src="" alt="Jiji Kenya">
-                <p></p>
-                <a href="">Visit Site</a>
-            </div>
-            <div class="markettype">
-                <h3>Farmers Market Kenya</h3>
-                <img src="" alt="Farmers Market Kenya">
-                <p></p>
-                <a href="">Visit Site</a>
-            </div>
-        </div>
-    </section>
-
-    <!-- footer -->
     <section class="footer">
             <div class="rowftr">
                 <div class="ftritm">
@@ -114,16 +68,7 @@
                 </div>
             </div>
     </section>
-    <!-- // javascript code for showing and closing the menu for small screnn devices -->
-    <script>
-    var nav_link=document.getElementById("nav_link");
-    function showMenu(){
-        nav_link.style.right='0';
-    }
-    function hideMenu(){
-        nav_link.style.right='-200px';
-    }
-
-    </script>
+    
+    
 </body>
 </html>
